ae812aa078a948d8347688b5bbb31bf1d0758d81
Dan Payne <dan@danpayne.info>
2025-01-21 18:54:57 -0700

    add 5 rows of items

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,60 @@
-import { useState } from 'react'
-import './App.css'
-
+import { useState } from "react";
+import "./App.css";
 
 const CalculatorPage = () => {
   const [value1, setValue1] = useState(0);
   const [value2, setValue2] = useState(0);
   const [value3, setValue3] = useState(0);
+  const [value4, setValue4] = useState(0);
+  const [value5, setValue5] = useState(0);
+  const [value6, setValue6] = useState(0);
+  const [value7, setValue7] = useState(0);
+  const [value8, setValue8] = useState(0);
   const [result, setResult] = useState(null);
 
   const numbers = Array.from({ length: 10 }, (_, i) => i); // Array [0, 1, 2, ..., 9]
 
   const handleCalculate = () => {
-    setResult(value1 * value2 * value3);
+    setResult(
+      value1 * value2 * value3 * value4 * value5 * value6 * value7 * value8
+    );
   };
 
   return (
     <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
       <h1>Calculator</h1>
-      <div style={{ marginBottom: "10px" }}>
-        <label>
-          Select Number 1:
-          <select
-            value={value1}
-            onChange={(e) => setValue1(Number(e.target.value))}
-            style={{ marginLeft: "10px", padding: "5px" }}
-          >
-            {numbers.map((num) => (
-              <option key={num} value={num}>
-                {num}
-              </option>
-            ))}
-          </select>
-        </label>
-      </div>
-
-      <div style={{ marginBottom: "10px" }}>
-        <label>
-          Select Number 2:
-          <select
-            value={value2}
-            onChange={(e) => setValue2(Number(e.target.value))}
-            style={{ marginLeft: "10px", padding: "5px" }}
-          >
-            {numbers.map((num) => (
-              <option key={num} value={num}>
-                {num}
-              </option>
-            ))}
-          </select>
-        </label>
-      </div>
-
-      <div style={{ marginBottom: "10px" }}>
-        <label>
-          Select Number 3:
-          <select
-            value={value3}
-            onChange={(e) => setValue3(Number(e.target.value))}
-            style={{ marginLeft: "10px", padding: "5px" }}
-          >
-            {numbers.map((num) => (
-              <option key={num} value={num}>
-                {num}
-              </option>
-            ))}
-          </select>
-        </label>
-      </div>
+      {[value1, value2, value3, value4, value5, value6, value7, value8].map(
+        (value, index) => (
+          <div style={{ marginBottom: "10px" }} key={index}>
+            <label>
+              Select Number {index + 1}:
+              <select
+                value={value}
+                onChange={(e) => {
+                  const setters = [
+                    setValue1,
+                    setValue2,
+                    setValue3,
+                    setValue4,
+                    setValue5,
+                    setValue6,
+                    setValue7,
+                    setValue8,
+                  ];
+                  setters[index](Number(e.target.value));
+                }}
+                style={{ marginLeft: "10px", padding: "5px" }}
+              >
+                {numbers.map((num) => (
+                  <option key={num} value={num}>
+                    {num}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </div>
+        )
+      )}
 
       <button
         onClick={handleCalculate}
@@ -98,4 +86,5 @@ export default function App() {
     </div>
   );
 }
+
 
